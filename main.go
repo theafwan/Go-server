fe29a1c74305a96f82e432e3f5d7861bfa0405a6
Mohammed Afwan <afwan.md2@gmail.com>
2023-07-22 18:55:42 +0200

    Indentation fix

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func formHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Parseform() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request Successful")
+	fmt.Fprintf(w, "POST request Successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
